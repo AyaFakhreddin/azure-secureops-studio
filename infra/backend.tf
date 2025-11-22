5db504e2784b42ef0de8b7027f14047aade92a3d
Aya Fakhreddia <aya.fakhreddin2003@gmail.com>
2025-11-22 17:23:23 +0100

    erreur from aya

--- infra/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "aoss-dev-rg-secops" 
-    storage_account_name = "aossdevstg2025"
-    container_name       = "tfstate"
-    key                  = "secureops/terraform.tfstate"
-  }
-}
--- infra/variables.tf
+++
@@ -1,9 +0,0 @@
-variable "location" {
-  type    = string
-  default = "France Central"
-}
-
-variable "project_name" {
-  type    = string
-  default = "azure-secureops-studio"
-}
