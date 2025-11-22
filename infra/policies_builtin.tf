d3c4822a515db08893acdd3d26d4db8fd18936ca
jihane elotmani <J232003>
2025-11-22 19:42:01 +0100

    built in policies

---
+++ infra/policies_builtin.tf
@@ -0,0 +1,61 @@
+####################################
+# 1. Récupérer les définitions BUILT-IN
+####################################
+
+# Allowed locations
+data "azurerm_policy_definition" "allowed_locations" {
+  name = "e56962a6-4747-9cd-b67b-bf8b01975c4c"
+}
+
+# Require a tag and its value on resources
+data "azurerm_policy_definition" "require_tag" {
+  name = "1e30110a-5ceb-460c-a204-c1c3969c6d62"
+}
+
+# Secure transfer for storage accounts
+data "azurerm_policy_definition" "secure_transfer" {
+  name = "404c3081-a854-4457-ae30-26a93ef643f9"
+}
+
+####################################
+# 2. Assignations au niveau Subscription
+####################################
+
+# 2.1 Allowed locations
+resource "azurerm_subscription_policy_assignment" "assign_allowed_locations" {
+  name                 = "aoss-assign-allowed-locations"
+  display_name         = "AOSS - Allowed locations"
+  subscription_id      = var.subscription_id
+  policy_definition_id = data.azurerm_policy_definition.allowed_locations.id
+
+  parameters = jsonencode({
+    listOfAllowedLocations = {
+      value = ["westeurope"]
+    }
+  })
+}
+
+# 2.2 Require tag Owner
+resource "azurerm_subscription_policy_assignment" "assign_require_tag_owner" {
+  name                 = "aoss-assign-require-tag-owner"
+  display_name         = "AOSS - Require tag Owner on resources"
+  subscription_id      = var.subscription_id
+  policy_definition_id = data.azurerm_policy_definition.require_tag.id
+
+  parameters = jsonencode({
+    tagName = {
+      value = "Owner"
+    }
+    tagValue = {
+      value = "NotSet"
+    }
+  })
+}
+
+# 2.3 Secure transfer for Storage Accounts
+resource "azurerm_subscription_policy_assignment" "assign_secure_transfer_storage" {
+  name                 = "aoss-assign-secure-transfer-storage"
+  display_name         = "AOSS - Secure transfer required for storage accounts"
+  subscription_id      = var.subscription_id
+  policy_definition_id = data.azurerm_policy_definition.secure_transfer.id
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -7,3 +7,8 @@ variable "project_name" {
   type    = string
   default = "azure-secureops-studio"
 }
+
+variable "subscription_id" {
+  type        = string
+  description = "the subscription id for the compte"
+}
