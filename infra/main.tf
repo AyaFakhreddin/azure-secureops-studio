656f3435e269b2258a997bfc6d1484f86b36309f
Aya Fakhreddia <aya.fakhreddin2003@gmail.com>
2025-12-09 18:09:41 +0100

    solving storage tag conflex

--- infra/main.tf
+++ infra/main.tf
@@ -23,12 +23,16 @@ resource "azurerm_storage_account" "st" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  tags = {
+    Owner = "AutoAssigned"
+  }
 }
 
 resource "azurerm_storage_container" "tfstate" {
   name                  = "tfstate"
   storage_account_name  = azurerm_storage_account.st.name
   container_access_type = "private"
+  
 }
 
 #test
